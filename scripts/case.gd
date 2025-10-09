a5ddbe270c6d3d70aa56a99774342349fbdc7184
ptchu2207 <ptchu2207@gmail.com>
2025-10-09 16:41:49 +0200

    kekchose

--- scripts/case.gd
+++ scripts/case.gd
@@ -1,5 +1,5 @@
 extends Button
-
+class_name Case
 @export var nom := "case"
 
 @onready var symbole := $Symbole
@@ -7,4 +7,13 @@ extends Button
 func _on_pressed() -> void:
 	print("On a appuyé sur ", nom, " !")
 	symbole.visible = true
+	
+	var tab = [0, 1, 2, 3]
+	
+	for i in range(4):
+		print(tab[i])
+	
 	pass # Replace with function body.
+
+func additionner(n1, n2):
+	return n1 + n2
