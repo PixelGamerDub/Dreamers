9bb9535e4e3211fa5d342e6cadf47c17c47171fc
ptchu2207 <ptchu2207@gmail.com>
2025-10-09 15:58:42 +0200

    kekchose

--- scripts/case.gd
+++ scripts/case.gd
@@ -2,6 +2,9 @@ extends Button
 
 @export var nom := "case"
 
+@onready var symbole := $Symbole
+
 func _on_pressed() -> void:
 	print("On a appuyé sur ", nom, " !")
+	symbole.visible = true
 	pass # Replace with function body.
