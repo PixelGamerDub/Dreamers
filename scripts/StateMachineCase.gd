b48c8bf9e16f52052716ddc45310dc39922a71dd
ptchu2207 <ptchu2207@gmail.com>
2025-10-23 18:25:43 +0200

    Poursuite de StateMachine(il faut finir StateMachineCase)

--- scripts/StateMachineCase.gd
+++ scripts/StateMachineCase.gd
@@ -1,14 +1,24 @@
-extends Node
-class_name StateMachine
+extends StateMachine
+class_name StateMachineCase
 
-var etatInitial : State
-var etatActuel : State
+@onready var etats = {
+	StateCase.ETAT_VIDE: 	$VideState,
+	StateCase.ETAT_FISSURE:	$FissureState,
+	StateCase.ETAT_DETRUIT:	$DetruitState,
+	StateCase.ETAT_REGEN:	$RegenState,
+	StateCase.ETAT_O:		$OState,
+	StateCase.ETAT_X:		$XState
+}
 
+func _init():
+	etatInitial = $VideState
+	etatActuel = etatInitial
+	
 func _ready() :
 	etatInitial.entrer()
 	
-func changerEtat(_move : String):
-	pass
-	
+func changerEtat(move : String):
+	if etatActuel.jouable :
+	#Reprendre ici
 func forcerEtat(_etat : State):
 	pass
